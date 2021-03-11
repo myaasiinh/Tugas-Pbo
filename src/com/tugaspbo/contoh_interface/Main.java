a247e8568f83e96f50e9aff7402de70c4a1c7561
myaasiinh <myaasiinh.com>
2021-03-11 13:42:31 +0700

    initial commit

--- src/com/tugaspbo/contoh_interface/Main.java
+++ src/com/tugaspbo/contoh_interface/Main.java
@@ -6,21 +6,43 @@ public class Main {
 
     public static void main(String[] args) {
 
-
         //Membuat objek Hp
         Phone Redmi8A = new Xiaomi();
-        PhoneUser sabil = new PhoneUser(Redmi8A);
+        PhoneUser Yudha = new PhoneUser(Redmi8A);
 
         //Coba nyalakan Hp
-        sabil  turnOnPhone();
-
-
+        Yudha.turnOnPhone();
 
         Scanner input = new Scanner(System.in);
         String aksi;
 
+        while(true) {
+
+            System.out.println("Aplikasi Interface");
+            System.out.println("1.Nyalakan Hp");
+            System.out.println("2.Matikan Hp");
+            System.out.println("3.Perbesar Volume");
+            System.out.println("4.Turunkun Volume");
+            System.out.println("5.Keluar");
+            System.out.println("Pilih aksi");
+            aksi = input.nextLine();
 
 
+            if(aksi.equalsIgnoreCase("1")) {
+                Yudha.turnOnPhone();
+            } else if (aksi.equalsIgnoreCase("2")) {
+                Yudha.turnOffPhone();
+            } else if (aksi.equalsIgnoreCase("3")) {
+                Yudha.turnUpPhone();
+            } else if (aksi.equalsIgnoreCase("4")) {
+                Yudha.turnDownPhone();
+            } else if (aksi.equalsIgnoreCase("5")) {
+                System exit;
+            } else {
+                System.out.println("Kamu salah pilih");
+            }
+
+        }
 
     }
 
